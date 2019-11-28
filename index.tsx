e83b370e9e5de9fb5958136dad047c7310ce3047
mottox2 <y.mottox2@gmail.com>
2019-11-29 00:01:03 +0900

    Add --filename option

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import arg from 'arg'
 import React from 'react'
 import { generate, Presentation } from 'jsx-presentation'
 import Slide1 from './slides/Slide1'
@@ -5,10 +6,15 @@ import Slide2 from './slides/Slide2'
 import Slide3 from './slides/Slide3'
 import Slide4 from './slides/Slide4'
 
+const args = arg({
+  '--filename': String,
+  '--extensions': String // babel-node
+})
+
 const config = {
   dryRun: false,
   outDir: 'results',
-  fileName: 'output'
+  fileName: args["--filename"]
 }
 
 generate(<Presentation>
