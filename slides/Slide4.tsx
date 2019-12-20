9b3ecd1faac33b8b28a83c3283acaabcfe9bbce6
mottox2 <y.mottox2@gmail.com>
2019-12-20 17:25:58 +0900

    Update jsx-presentation to 0.0.22
    Use nested text and margin property

--- slides/Slide4.tsx
+++ slides/Slide4.tsx
@@ -39,13 +39,13 @@ export default () => (
       <Fragment flexDirection="row" flexGrow={1}>
         <Fragment flexGrow={1}>
           <SubHeader>Sub Header 1</SubHeader>
-          <Chart data={chartData} flexGrow={1} />
+          <Chart data={chartData} flexGrow={1} marginTop={2} />
         </Fragment>
 
         <Fragment flexGrow={1}>
           <SubHeader>Sub Header 2</SubHeader>
-          <Text {...bodyStyle} flexGrow={1}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <Text {...bodyStyle} flexGrow={1} marginTop={2}>
+            Lorem ipsum dolor sit amet, <Text bold>consectetur adipisicing</Text> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Text>
         </Fragment>
       </Fragment>
